5752465cdf6d697702a92ee993684cdc12c2efd7
rickyfun <r.huang850@gmail.com>
2019-04-09 14:34:40 +0800

    Add mails page

--- src/customization/mails.vue
+++ src/customization/mails.vue
@@ -2,7 +2,7 @@
   <div class="animated fadeIn">
     <b-row>
       <b-col sm="12">
-        <c-table :table-data="itemsArray" :per-page=10 hover striped bordered small fixed caption="<i class='fa fa-align-justify'></i> Combined All Table"></c-table>
+        <c-table :table-data="itemsArray" :per-page=10 hover striped bordered small fixed caption="Mails List"></c-table>
       </b-col>
     </b-row>
   </div>
@@ -10,31 +10,35 @@
 </template>
 
 <script>
-import { shuffleArray } from '@/shared/utils'
 import cTable from '@/views/base/Table.vue'
-
-const someData = () => shuffleArray([
-  {username: 'Samppa Nori', registered: '2012/01/01', role: 'Member', status: 'Active', _rowVariant: 'success'},
-  {username: 'Estavan Lykos', registered: '2012/02/01', role: 'Staff', status: 'Banned', _rowVariant: 'danger'},
-  {username: 'Chetan Mohamed', registered: '2012/02/01', role: 'Admin', status: 'Inactive', _rowVariant: 'info'},
-  {username: 'Derick Maximinus', registered: '2012/03/01', role: 'Member', status: 'Pending'},
-  {username: 'Friderik Dávid', registered: '2012/01/21', role: 'Staff', status: 'Active'},
-])
 
 export default {
   name: 'tables',
   components: {cTable},
   data: () => {
     return {
-      items: someData,
-      itemsArray: someData(),
-      fields: [
-        {key: 'username', label: 'User', sortable: true},
-        {key: 'registered'},
-        {key: 'role'},
-        {key: 'status', sortable: true}
-      ],
+      // items: ,
+      itemsArray: [],
+      fields: []
     }
+  },
+  created() {
+    this.$apiRequest.get('api/v1/mail',)
+    .then(response => {
+      let maillist = new Array
+      response.data.forEach(element => {
+        maillist.push({
+          owner_name: element.owner_name,
+          owner_e_name: element.owner_e_name,
+          name: element.name,
+          org_name: element.org_name,
+          desc: element.desc
+          })
+      });
+      this.itemsArray = maillist
+    }).catch(error => {
+      console.log(error)
+    })
   }
 }
 </script>
