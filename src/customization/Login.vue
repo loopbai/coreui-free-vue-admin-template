ba88ff025a2f6d7186b17abfb7e0ccdfdb417787
rickyfun <r.huang850@gmail.com>
2019-04-09 16:49:32 +0800

    Add Login page

--- src/customization/Login.vue
+++ src/customization/Login.vue
@@ -11,15 +11,15 @@
                   <p class="text-muted">Sign In to your account</p>
                   <b-input-group class="mb-3">
                     <b-input-group-prepend><b-input-group-text><i class="icon-user"></i></b-input-group-text></b-input-group-prepend>
-                    <b-form-input type="text" class="form-control" placeholder="Username" autocomplete="username email" />
+                    <b-form-input type="text" class="form-control" placeholder="Username" autocomplete="username email" v-model="input.username" />
                   </b-input-group>
                   <b-input-group class="mb-4">
                     <b-input-group-prepend><b-input-group-text><i class="icon-lock"></i></b-input-group-text></b-input-group-prepend>
-                    <b-form-input type="password" class="form-control" placeholder="Password" autocomplete="current-password" />
+                    <b-form-input type="password" class="form-control" placeholder="Password" autocomplete="current-password" v-model="input.password" />
                   </b-input-group>
                   <b-row>
                     <b-col cols="6">
-                      <b-button variant="primary" class="px-4">Login</b-button>
+                      <b-button variant="primary" class="px-4" v-on:click="login()">Login</b-button>
                     </b-col>
                     <b-col cols="6" class="text-right">
                       <b-button variant="link" class="px-0">Forgot password?</b-button>
@@ -28,7 +28,7 @@
                 </b-form>
               </b-card-body>
             </b-card>
-            <b-card no-body class="text-white bg-primary py-5 d-md-down-none" style="width:44%">
+            <!-- <b-card no-body class="text-white bg-primary py-5 d-md-down-none" style="width:44%">
               <b-card-body class="text-center">
                 <div>
                   <h2>Sign up</h2>
@@ -36,7 +36,7 @@
                   <b-button variant="primary" class="active mt-3">Register Now!</b-button>
                 </div>
               </b-card-body>
-            </b-card>
+            </b-card> -->
           </b-card-group>
         </b-col>
       </b-row>
@@ -45,7 +45,38 @@
 </template>
 
 <script>
+import router from '../router'
+import Vue from 'vue'
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  data() {
+    return {
+      input: {
+        username: "",
+        password: ""
+      }
+    }
+  },
+  methods: {
+    login() {
+      if(this.input.username != "" && this.input.password != "") {
+        Vue.prototype.$apiRequest.post('api/v1/login', {
+          'email': this.input.username,
+          'password': this.input.password
+        }).then(function(response) {
+          if (response.data.token) {
+            localStorage.setItem('token', response.data.token)
+            Vue.prototype.$apiRequest.defaults.headers.common['Authorization'] = 'Bearer ' + response.data.token;
+          }
+          router.replace({ name: "Home"});
+        }).catch(function(error) {
+          console.log(error)
+        })
+      } else {
+        console.log("A username and password must be present");
+      }
+    }
+  }
 }
 </script>
